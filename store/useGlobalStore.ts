4b4dafd31880e4287c5297b737564433604ad8d0
iLia Railian <raylyanway@mail.ru>
2025-05-02 23:04:49 +0000

    add global store

---
+++ store/useGlobalStore.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+
+import { themedColors } from '@/config';
+
+import { Theme, ThemedColors } from '@/types';
+
+interface State {
+  computed: {
+    colors: ThemedColors;
+    isLightTheme: boolean;
+    oppositeColors: ThemedColors;
+  };
+  setTheme: (theme: Theme) => void;
+  theme: Theme;
+}
+
+export const useGlobalStore = create<State>()((set, get) => ({
+  computed: {
+    get colors() {
+      return get().theme === 'light' ? themedColors.light : themedColors.dark;
+    },
+    get oppositeColors() {
+      return get().theme === 'light' ? themedColors.dark : themedColors.light;
+    },
+    get isLightTheme() {
+      return get().theme === 'light';
+    }
+  },
+  setTheme: (theme: Theme) => set({ theme }),
+  theme: 'light' as const,
+}));
