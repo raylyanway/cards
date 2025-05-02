2af39324e1167d556a4c1d5ec4d4dbcf019cce16
iLia Railian <raylyanway@mail.ru>
2025-05-02 23:04:40 +0000

    add types

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+import { Ionicons } from '@expo/vector-icons';
+import { ComponentProps } from 'react';
+
+import { fontSizes, palette, themedColors } from '@/config';
+
+export type IoniconsProps = ComponentProps<typeof Ionicons>;
+export type IconName = IoniconsProps['name'];
+export type Theme = keyof typeof themedColors;
+export type ThemedColors = typeof themedColors.dark;
+export type ThemedColor = keyof typeof themedColors.dark;
+export type PaletteColor = keyof typeof palette;
+export type ColorName = ThemedColor | PaletteColor;
+export type FontSize = keyof typeof fontSizes;
