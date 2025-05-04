2a4dff023024b78031a3623455206d85703e6599
iLia Railian <raylyanway@mail.ru>
2025-05-04 21:40:37 +0000

    add prettier and sort import to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,38 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
-    ignores: ['dist/*'],
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: { 'simple-import-sort': simpleImportSort },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Packages `react` related packages come first.
+            ['^react', '^@?\\w'],
+            // Side effect imports.
+            ['^\\u0000'],
+            // Internal packages.
+            ['^(@|components)(/.*|$)'],
+            // Parent imports. Put `..` last.
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            // Other relative imports. Put same-folder imports and `.` last.
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            // Style imports.
+            ['^.+\\.?(css)$']
+          ]
+        }
+      ]
+    }
   },
+  {
+    ignores: ['dist/*']
+  }
 ]);
