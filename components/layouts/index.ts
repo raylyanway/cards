e75605fa048561b90538a6f447b53c60efaf8dc6
iLia Railian <raylyanway@mail.ru>
2025-05-05 20:19:44 +0000

    remove List component

--- components/layouts/index.ts
+++ components/layouts/index.ts
@@ -1,7 +1,6 @@
 export * from './Block';
 export * from './BlockList';
 export * from './Center';
-export * from './List';
 export * from './Padding';
 export * from './ScrollView';
 export * from './View';
