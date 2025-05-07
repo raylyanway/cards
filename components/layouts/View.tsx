635e43ce140a0d2336ad60e560d18c2bbea23df4
iLia Railian <raylyanway@mail.ru>
2025-05-07 20:07:22 +0000

    Update View

--- components/layouts/View.tsx
+++ components/layouts/View.tsx
@@ -1,14 +1,37 @@
-import { View as RNView, ViewProps as RNViewProps } from 'react-native';
+import {
+  View as RNView,
+  ViewProps as RNViewProps,
+  StyleSheet
+} from 'react-native';
 
+import { spaces } from '@/config';
 import { useGlobalStore } from '@/store/useGlobalStore';
 
 export interface ViewProps extends RNViewProps {
   themed?: boolean;
+  row?: boolean;
 }
 
-export const View = ({ style, themed = false, ...viewProps }: ViewProps) => {
+export const View = ({
+  style,
+  themed = false,
+  row = false,
+  ...viewProps
+}: ViewProps) => {
   const colors = useGlobalStore((s) => s.computed.colors);
   const backgroundColor = themed ? colors.background : undefined;
+  const rowStyle = row ? styles.row : undefined;
 
-  return <RNView style={[{ backgroundColor }, style]} {...viewProps} />;
+  return (
+    <RNView style={[{ backgroundColor }, rowStyle, style]} {...viewProps} />
+  );
 };
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: spaces.md
+  }
+});
