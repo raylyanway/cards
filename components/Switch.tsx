47eb4cb5e387851fced6b21ef3bee38502b03565
iLia Railian <raylyanway@mail.ru>
2025-05-07 23:47:25 +0000

    update Switch

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -2,11 +2,15 @@ import { Switch as RNSwitch, SwitchProps } from 'react-native';
 
 import { palette } from '@/config';
 
-export const Switch = (switchProps: SwitchProps) => {
+export const Switch = ({ value, ...switchProps }: SwitchProps) => {
+  const iosBackgroundColor = value ? palette.green : palette.grayDark;
+
   return (
     <RNSwitch
       trackColor={{ false: palette.gray, true: palette.green }}
       thumbColor={palette.white}
+      ios_backgroundColor={iosBackgroundColor}
+      value={value}
       {...switchProps}
     />
   );
