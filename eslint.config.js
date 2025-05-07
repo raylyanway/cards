845bac0c8f45432292f633a4352c91d2fd0369bf
iLia Railian <raylyanway@mail.ru>
2025-05-07 20:06:51 +0000

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,28 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
-const simpleImportSort = require('eslint-plugin-simple-import-sort');
 
 module.exports = defineConfig([
   expoConfig,
   {
     files: ['**/*.ts', '**/*.tsx'],
-    plugins: { 'simple-import-sort': simpleImportSort },
     rules: {
-      'simple-import-sort/imports': [
+      'import/order': [
         'error',
         {
           groups: [
-            // Packages `react` related packages come first.
-            ['^@?\\w'],
-            // Side effect imports.
-            ['^\\u0000'],
-            // Internal packages.
-            ['^(@|components)(/.*|$)'],
-            // Parent imports. Put `..` last.
-            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-            // Other relative imports. Put same-folder imports and `.` last.
-            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-            // Style imports.
-            ['^.+\\.?(css)$']
-          ]
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index'
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          }
         }
       ]
     }
