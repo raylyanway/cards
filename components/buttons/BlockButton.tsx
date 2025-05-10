8f494971fc1514fe14d1c8cf184c1c06745cd60d
iLia Railian <raylyanway@mail.ru>
2025-05-10 03:18:37 +0000

    add BlockButton

---
+++ components/buttons/BlockButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Block, BlockProps } from '@/components/layouts/Block';
+import { Text, TextProps } from '@/components/texts/Text';
+
+import { Button, ButtonProps } from './Button';
+
+interface BlockButtonProps extends ButtonProps {
+  blockProps?: BlockProps;
+  textProps?: TextProps;
+  fullWidth?: boolean;
+}
+
+export const BlockButton = ({
+  children,
+  blockProps,
+  textProps,
+  fullWidth = false,
+  style,
+  ...buttonProps
+}: BlockButtonProps) => {
+  const blockStyle = fullWidth ? styles.fullWidth : undefined;
+
+  return (
+    <Button style={[styles.default, blockStyle, style]} {...buttonProps}>
+      <Block {...blockProps}>
+        <Text {...textProps}>{children}</Text>
+      </Block>
+    </Button>
+  );
+};
+
+const styles = StyleSheet.create({
+  default: {
+    alignSelf: 'flex-start'
+  },
+  fullWidth: {
+    alignSelf: 'stretch'
+  }
+});
