f8325383447dcaede3072845dbfd541c3a5238f7
iLia Railian <raylyanway@mail.ru>
2025-05-10 03:20:02 +0000

    update CardsScreen to enhance layout with Padding and BlockButton components

--- app/(tabs)/cards.tsx
+++ app/(tabs)/cards.tsx
@@ -1,12 +1,32 @@
+import { BlockButton } from '@/components/buttons/BlockButton';
+import { Center } from '@/components/layouts/Center';
+import { Padding } from '@/components/layouts/Padding';
 import { SafeAreaView } from '@/components/layouts/SafeAreaView';
 import { ScrollView } from '@/components/layouts/ScrollView';
+import { View } from '@/components/layouts/View';
 import { Text } from '@/components/texts/Text';
 
 export default function CardsScreen() {
   return (
     <SafeAreaView themed fullScreen>
-      <ScrollView>
-        <Text>Cards</Text>
+      <ScrollView tabPadding fullScreen>
+        <Padding fullScreen style={{ borderColor: 'green', borderWidth: 1 }}>
+          <View row>
+            <Text>verb</Text>
+            <Text>бии</Text>
+          </View>
+          <View row>
+            <Text>base</Text>
+            <Text>A1 (#1)</Text>
+          </View>
+          <Center style={{ borderColor: 'green', borderWidth: 1 }}>
+            <Text type="title">be</Text>
+          </Center>
+          <View row style={{ justifyContent: 'center' }}>
+            <BlockButton>prev</BlockButton>
+            <BlockButton>next</BlockButton>
+          </View>
+        </Padding>
       </ScrollView>
     </SafeAreaView>
   );
