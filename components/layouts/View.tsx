f798f6709fd396d749bed5f2e9abb585e07de82e
iLia Railian <raylyanway@mail.ru>
2025-05-10 03:07:12 +0000

    update View

--- components/layouts/View.tsx
+++ components/layouts/View.tsx
@@ -10,20 +10,26 @@ import { useGlobalStore } from '@/store/useGlobalStore';
 export interface ViewProps extends RNViewProps {
   themed?: boolean;
   row?: boolean;
+  fullScreen?: boolean;
 }
 
 export const View = ({
   style,
   themed = false,
   row = false,
+  fullScreen = false,
   ...viewProps
 }: ViewProps) => {
   const colors = useGlobalStore((s) => s.computed.colors);
   const backgroundColor = themed ? colors.background : undefined;
   const rowStyle = row ? styles.row : undefined;
+  const flex = fullScreen ? 1 : undefined;
 
   return (
-    <RNView style={[{ backgroundColor }, rowStyle, style]} {...viewProps} />
+    <RNView
+      style={[{ backgroundColor, flex }, rowStyle, style]}
+      {...viewProps}
+    />
   );
 };
 
