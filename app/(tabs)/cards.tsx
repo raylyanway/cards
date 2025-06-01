dcba232b6a2c8fd0da4f17e31ef54a52333171d1
iLia Railian <raylyanway@mail.ru>
2025-06-01 00:36:11 +0000

    add Speech functionality to CardsScreen and integrate expo-speech dependency

--- app/(tabs)/cards.tsx
+++ app/(tabs)/cards.tsx
@@ -1,4 +1,7 @@
+import * as Speech from 'expo-speech';
+
 import { BlockButton } from '@/components/buttons/BlockButton';
+import { IconButton } from '@/components/buttons/IconButton';
 import { Center } from '@/components/layouts/Center';
 import { Padding } from '@/components/layouts/Padding';
 import { SafeAreaView } from '@/components/layouts/SafeAreaView';
@@ -12,12 +15,20 @@ export default function CardsScreen() {
       <ScrollView tabPadding fullScreen>
         <Padding fullScreen style={{ borderColor: 'green', borderWidth: 1 }}>
           <Meta />
-          <Word />
+          <Word word="be" />
           <Controls />
         </Padding>
       </ScrollView>
     </SafeAreaView>
   );
+}
+
+function SpeakButton({ text }: { text: string }) {
+  const speak = () => {
+    Speech.speak(text);
+  };
+
+  return <IconButton iconProps={{ name: 'volume-medium' }} onPress={speak} />;
 }
 
 function Controls() {
@@ -44,11 +55,14 @@ function Meta() {
   );
 }
 
-function Word() {
+function Word({ word }: { word: string }) {
   return (
     <Center style={{ borderColor: 'green', borderWidth: 1 }}>
-      <Text type="title">be</Text>
-      <Text type="defaultSecondary">/biː/</Text>
+      <Text type="title">{word}</Text>
+      <View row>
+        <Text type="defaultSecondary">/biː/</Text>
+        <SpeakButton text={word} />
+      </View>
       <Text type="subtitle">быть</Text>
     </Center>
   );
