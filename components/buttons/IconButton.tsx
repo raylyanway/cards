27fa70a9b93f110dba7a3ade02511828c80644b5
iLia Railian <raylyanway@mail.ru>
2025-06-01 19:57:08 +0000

    refactor IconButton to enhance icon rendering; make iconProps optional and integrate name prop for improved flexibility

--- components/buttons/IconButton.tsx
+++ components/buttons/IconButton.tsx
@@ -3,13 +3,18 @@ import { Icon, IconProps } from '@/components/icons/Icon';
 import { Button, ButtonProps } from './Button';
 
 interface IconButtonProps extends ButtonProps {
-  iconProps: IconProps;
+  name: IconProps['name'];
+  iconProps?: Omit<IconProps, 'name'>;
 }
 
-export const IconButton = ({ iconProps, ...buttonProps }: IconButtonProps) => {
+export const IconButton = ({
+  name,
+  iconProps,
+  ...buttonProps
+}: IconButtonProps) => {
   return (
     <Button {...buttonProps}>
-      <Icon {...iconProps} />
+      <Icon name={name} {...iconProps} />
     </Button>
   );
 };
