7c0fe997adc037fe687bb01c23814f6bd893a459
iLia Railian <raylyanway@mail.ru>
2025-06-01 04:00:19 +0000

    refactor CardsScreen to manage word state and enhance WordCard component with dynamic content; update Controls to handle navigation

--- app/(tabs)/cards.tsx
+++ app/(tabs)/cards.tsx
@@ -1,4 +1,5 @@
 import * as Speech from 'expo-speech';
+import { useState } from 'react';
 
 import { BlockButton } from '@/components/buttons/BlockButton';
 import { IconButton } from '@/components/buttons/IconButton';
@@ -9,14 +10,47 @@ import { ScrollView } from '@/components/layouts/ScrollView';
 import { View } from '@/components/layouts/View';
 import { Text } from '@/components/texts/Text';
 
+interface Word {
+  word: string;
+  ipa: string;
+  translation: string;
+}
+
+const words: Word[] = [
+  { word: 'be', ipa: '/biː/', translation: 'быть' },
+  { word: 'have', ipa: '/hæv/', translation: 'иметь' },
+  { word: 'do', ipa: '/duː/', translation: 'делать' },
+  { word: 'say', ipa: '/seɪ/', translation: 'сказать' },
+  { word: 'go', ipa: '/ɡoʊ/', translation: 'идти' },
+  { word: 'get', ipa: '/ɡet/', translation: 'получать' },
+  { word: 'make', ipa: '/meɪk/', translation: 'делать' },
+  { word: 'know', ipa: '/noʊ/', translation: 'знать' },
+  { word: 'think', ipa: '/θɪŋk/', translation: 'думать' },
+  { word: 'take', ipa: '/teɪk/', translation: 'брать' }
+];
+
 export default function CardsScreen() {
+  const [index, setIndex] = useState(0);
+  const currentWord = words[index];
+
+  const handleNextPress = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % words.length);
+  };
+
+  const handlePrevPress = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + words.length) % words.length);
+  };
+
   return (
     <SafeAreaView themed fullScreen>
       <ScrollView tabPadding fullScreen>
         <Padding fullScreen>
           <Meta />
-          <WordCard word="be" />
-          <Controls />
+          <WordCard word={currentWord} />
+          <Controls
+            onNextPress={handleNextPress}
+            onPrevPress={handlePrevPress}
+          />
         </Padding>
       </ScrollView>
     </SafeAreaView>
@@ -31,11 +65,17 @@ function SpeakButton({ text }: { text: string }) {
   return <IconButton iconProps={{ name: 'volume-medium' }} onPress={speak} />;
 }
 
-function Controls() {
+function Controls({
+  onNextPress,
+  onPrevPress
+}: {
+  onNextPress: () => void;
+  onPrevPress: () => void;
+}) {
   return (
     <View row style={{ justifyContent: 'center' }}>
-      <BlockButton>prev</BlockButton>
-      <BlockButton>next</BlockButton>
+      <BlockButton onPress={onPrevPress}>prev</BlockButton>
+      <BlockButton onPress={onNextPress}>next</BlockButton>
     </View>
   );
 }
@@ -55,16 +95,16 @@ function Meta() {
   );
 }
 
-function WordCard({ word }: { word: string }) {
+function WordCard({ word: { word, ipa, translation } }: { word: Word }) {
   return (
     <Center>
       <Text type="title">{word}</Text>
       <View row style={{ paddingTop: 5 }}>
-        <Text type="defaultSecondary">/biː/</Text>
+        <Text type="defaultSecondary">{ipa}</Text>
         <SpeakButton text={word} />
       </View>
       <Text type="subtitle" style={{ paddingTop: 20 }}>
-        быть
+        {translation}
       </Text>
     </Center>
   );
