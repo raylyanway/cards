7d7f0d418361f0568bcde93d1555b705763076b0
iLia Railian <raylyanway@mail.ru>
2025-06-08 19:32:55 +0000

    fix: enhance Button component to support both animated and non-animated states

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -1,7 +1,18 @@
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import {
+  Pressable,
+  PressableProps,
+  TouchableOpacity,
+  TouchableOpacityProps
+} from 'react-native';
 
-export type ButtonProps = TouchableOpacityProps;
+export type AnimatedButtonProps = { animated: true } & TouchableOpacityProps;
+export type NonAnimatedButtonProps = { animated?: false } & PressableProps;
 
-export const Button = ({ children, ...buttonProps }: ButtonProps) => {
-  return <TouchableOpacity {...buttonProps}>{children}</TouchableOpacity>;
-};
+export type ButtonProps = AnimatedButtonProps | NonAnimatedButtonProps;
+
+export const Button = ({ animated, ...rest }: ButtonProps) =>
+  animated ? (
+    <TouchableOpacity {...(rest as TouchableOpacityProps)} />
+  ) : (
+    <Pressable {...(rest as PressableProps)} />
+  );
