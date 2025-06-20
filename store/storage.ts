9997215dba4022bcad0288ca87f68858ea3066b9
iLia Railian <raylyanway@mail.ru>
2025-06-20 02:05:16 +0000

    refactor: remove learned cards storage functions to streamline storage management

--- store/storage.ts
+++ store/storage.ts
@@ -1,24 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const LEARNED_CARDS_KEY = 'LEARNED_CARDS';
-
-export async function getLearnedCards(): Promise<number[]> {
-  const json = await AsyncStorage.getItem(LEARNED_CARDS_KEY);
-  return json ? JSON.parse(json) : [];
-}
-
-export async function setLearnedCards(ids: number[]): Promise<void> {
-  await AsyncStorage.setItem(LEARNED_CARDS_KEY, JSON.stringify(ids));
-}
-
-export async function addLearnedCard(id: number): Promise<void> {
-  const ids = await getLearnedCards();
-  if (!ids.includes(id)) {
-    ids.push(id);
-    await setLearnedCards(ids);
-  }
-}
-
 export async function setStorage(data: any): Promise<void> {
   await AsyncStorage.setItem('DATA', JSON.stringify(data));
 }
