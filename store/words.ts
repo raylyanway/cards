53f24ab64a0a73961ce8a08a08925fc3f5803755
iLia Railian <raylyanway@mail.ru>
2025-07-06 06:17:45 +0000

    feat: add words_1_100 data file for foundational vocabulary support

--- store/words.ts
+++ store/words.ts
@@ -6,7 +6,7 @@ export const words: ICard[] = [
     category: ['A1'],
     meta: ['verb', 'бии', 'глагол', ''],
     content: [
-      { type: 'title', text: 'bebebebebebebebebebebebebebe', speak: true },
+      { type: 'title', text: 'be', speak: true },
       { type: 'defaultSecondary', text: '/biː/' },
       { type: 'defaultSemiBold', text: 'быть', hide: true }
     ]
@@ -14,11 +14,493 @@ export const words: ICard[] = [
   {
     id: 2,
     category: ['A1'],
+    meta: ['article', 'зе', 'артикль', ''],
+    content: [
+      { type: 'title', text: 'the', speak: true },
+      { type: 'defaultSecondary', text: '/ðə/' },
+      { type: 'defaultSemiBold', text: 'артикль определённый', hide: true }
+    ]
+  },
+  {
+    id: 3,
+    category: ['A1'],
+    meta: ['pronoun', 'ю', 'местоимение', ''],
+    content: [
+      { type: 'title', text: 'you', speak: true },
+      { type: 'defaultSecondary', text: '/juː/' },
+      { type: 'defaultSemiBold', text: 'ты, вы, тебя, вас, тебе, вам' }
+    ]
+  },
+  {
+    id: 4,
+    category: ['A1'],
+    meta: ['preposition', 'ту', 'предлог', ''],
+    content: [
+      { type: 'title', text: 'to', speak: true },
+      { type: 'defaultSecondary', text: '/tuː/' },
+      { type: 'defaultSemiBold', text: 'к, в, на, до, по, для' }
+    ]
+  },
+  {
+    id: 5,
+    category: ['A1'],
+    meta: ['conjunction', 'энд', 'союз', ''],
+    content: [
+      { type: 'title', text: 'and', speak: true },
+      { type: 'defaultSecondary', text: '/ænd/' },
+      { type: 'defaultSemiBold', text: 'и' }
+    ]
+  },
+  {
+    id: 6,
+    category: ['A1'],
+    meta: ['preposition', 'ов', 'предлог', ''],
+    content: [
+      { type: 'title', text: 'of', speak: true },
+      { type: 'defaultSecondary', text: '/əv/' },
+      { type: 'defaultSemiBold', text: 'из, от, с, относительно, по' }
+    ]
+  },
+  {
+    id: 7,
+    category: ['A1'],
+    meta: ['preposition', 'ин', 'предлог', ''],
+    content: [
+      { type: 'title', text: 'in', speak: true },
+      { type: 'defaultSecondary', text: '/ɪn/' },
+      { type: 'defaultSemiBold', text: 'в, на, внутри, во, при' }
+    ]
+  },
+  {
+    id: 8,
+    category: ['A1'],
+    meta: ['conjunction', 'зэт', 'союз', ''],
+    content: [
+      { type: 'title', text: 'that', speak: true },
+      { type: 'defaultSecondary', text: '/ðæt/' },
+      { type: 'defaultSemiBold', text: 'что, который, та, тот, те, это' }
+    ]
+  },
+  {
+    id: 9,
+    category: ['A1'],
     meta: ['verb', 'хэв', 'глагол', ''],
     content: [
       { type: 'title', text: 'have', speak: true },
       { type: 'defaultSecondary', text: '/hæv/' },
-      { type: 'defaultSemiBold', text: 'иметь', hide: true }
+      {
+        type: 'defaultSemiBold',
+        text: 'иметь, обладать, есть, получать, владеть'
+      }
+    ]
+  },
+  {
+    id: 10,
+    category: ['A1'],
+    meta: ['pronoun', 'ит', 'местоимение', ''],
+    content: [
+      { type: 'title', text: 'it', speak: true },
+      { type: 'defaultSecondary', text: '/ɪt/' },
+      { type: 'defaultSemiBold', text: 'это, оно, ему, её, его, им' }
+    ]
+  },
+  {
+    id: 11,
+    category: ['A1'],
+    meta: ['verb', 'ду', 'глагол', ''],
+    content: [
+      { type: 'title', text: 'do', speak: true },
+      { type: 'defaultSecondary', text: '/duː/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'делать, выполнять, совершать, поступать, действовать'
+      }
+    ]
+  },
+  {
+    id: 12,
+    category: ['A1'],
+    meta: ['verb', 'гоу', 'глагол', ''],
+    content: [
+      { type: 'title', text: 'go', speak: true },
+      { type: 'defaultSecondary', text: '/ɡoʊ/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'идти, ехать, ходить, двигаться, уходить'
+      }
+    ]
+  },
+  {
+    id: 13,
+    category: ['A1'],
+    meta: ['pronoun', 'май', 'местоимение', ''],
+    content: [
+      { type: 'title', text: 'my', speak: true },
+      { type: 'defaultSecondary', text: '/maɪ/' },
+      { type: 'defaultSemiBold', text: 'мой, моя, моё, мои, мне, мной' }
+    ]
+  },
+  {
+    id: 14,
+    category: ['A1'],
+    meta: ['verb', 'гет', 'глагол', ''],
+    content: [
+      { type: 'title', text: 'get', speak: true },
+      { type: 'defaultSecondary', text: '/ɡet/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'получать, получать, добираться, становиться, приобретать'
+      }
+    ]
+  },
+  {
+    id: 15,
+    category: ['A1'],
+    meta: ['pronoun', 'ми', 'местоимение', ''],
+    content: [
+      { type: 'title', text: 'me', speak: true },
+      { type: 'defaultSecondary', text: '/miː/' },
+      { type: 'defaultSemiBold', text: 'меня, мне, мной, мне, меня' }
+    ]
+  },
+  {
+    id: 16,
+    category: ['A1'],
+    meta: ['preposition', 'фор', 'предлог', ''],
+    content: [
+      { type: 'title', text: 'for', speak: true },
+      { type: 'defaultSecondary', text: '/fɔːr/' },
+      { type: 'defaultSemiBold', text: 'для, за, к, на, в пользу' }
+    ]
+  },
+  {
+    id: 17,
+    category: ['A1'],
+    meta: ['preposition', 'он', 'предлог', ''],
+    content: [
+      { type: 'title', text: 'on', speak: true },
+      { type: 'defaultSecondary', text: '/ɒn/' },
+      { type: 'defaultSemiBold', text: 'на, в, по, при, о' }
+    ]
+  },
+  {
+    id: 18,
+    category: ['A1'],
+    meta: ['pronoun', 'зис', 'местоимение', ''],
+    content: [
+      { type: 'title', text: 'this', speak: true },
+      { type: 'defaultSecondary', text: '/ðɪs/' },
+      { type: 'defaultSemiBold', text: 'это, этот, эта, эти, этому, этим' }
+    ]
+  },
+  {
+    id: 19,
+    category: ['A1'],
+    meta: ['pronoun', 'ви', 'местоимение', ''],
+    content: [
+      { type: 'title', text: 'we', speak: true },
+      { type: 'defaultSecondary', text: '/wiː/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'мы, нам, нас, нами, наш, наша, наше, наши'
+      }
+    ]
+  },
+  {
+    id: 20,
+    category: ['A1'],
+    meta: ['verb', 'кэн', 'глагол', ''],
+    content: [
+      { type: 'title', text: 'can', speak: true },
+      { type: 'defaultSecondary', text: '/kæn/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'мочь, уметь, быть в состоянии, мочь, смочь'
+      }
+    ]
+  },
+  {
+    id: 21,
+    category: ['A1'],
+    meta: ['preposition', 'вит', 'предлог', ''],
+    content: [
+      { type: 'title', text: 'with', speak: true },
+      { type: 'defaultSecondary', text: '/wɪð/' },
+      { type: 'defaultSemiBold', text: 'с, вместе с, при, за, от' }
+    ]
+  },
+  {
+    id: 22,
+    category: ['A1'],
+    meta: ['conjunction', 'бат', 'союз', ''],
+    content: [
+      { type: 'title', text: 'but', speak: true },
+      { type: 'defaultSecondary', text: '/bʌt/' },
+      { type: 'defaultSemiBold', text: 'но, а, однако, только, же' }
+    ]
+  },
+  {
+    id: 23,
+    category: ['A1'],
+    meta: ['verb', 'ноу', 'глагол', ''],
+    content: [
+      { type: 'title', text: 'know', speak: true },
+      { type: 'defaultSecondary', text: '/noʊ/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'знать, знать, узнавать, понимать, осознавать'
+      }
+    ]
+  },
+  {
+    id: 24,
+    category: ['A1'],
+    meta: ['pronoun', 'уот', 'местоимение', ''],
+    content: [
+      { type: 'title', text: 'what', speak: true },
+      { type: 'defaultSecondary', text: '/wɒt/' },
+      { type: 'defaultSemiBold', text: 'что, какой, что за, что такое, как' }
+    ]
+  },
+  {
+    id: 25,
+    category: ['A1'],
+    meta: ['adverb', 'ол', 'наречие', ''],
+    content: [
+      { type: 'title', text: 'all', speak: true },
+      { type: 'defaultSecondary', text: '/ɔːl/' },
+      { type: 'defaultSemiBold', text: 'всё, все, весь, вся, всё, все' }
+    ]
+  },
+  {
+    id: 26,
+    category: ['A1'],
+    meta: ['adverb', 'со', 'наречие', ''],
+    content: [
+      { type: 'title', text: 'so', speak: true },
+      { type: 'defaultSecondary', text: '/soʊ/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'так, настолько, так что, поэтому, таким образом'
+      }
+    ]
+  },
+  {
+    id: 27,
+    category: ['A1'],
+    meta: ['adverb', 'нот', 'наречие', ''],
+    content: [
+      { type: 'title', text: 'not', speak: true },
+      { type: 'defaultSecondary', text: '/nɒt/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'не, нет, не является, не быть, не делать'
+      }
+    ]
+  },
+  {
+    id: 28,
+    category: ['A1'],
+    meta: ['adverb', 'джаст', 'наречие', ''],
+    content: [
+      { type: 'title', text: 'just', speak: true },
+      { type: 'defaultSecondary', text: '/dʒʌst/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'просто, только что, только, лишь, едва'
+      }
+    ]
+  },
+  {
+    id: 29,
+    category: ['A1'],
+    meta: ['adverb', 'лайк', 'наречие', ''],
+    content: [
+      { type: 'title', text: 'like', speak: true },
+      { type: 'defaultSecondary', text: '/laɪk/' },
+      { type: 'defaultSemiBold', text: 'как, подобно, нравится, любить, вроде' }
+    ]
+  },
+  {
+    id: 30,
+    category: ['A1'],
+    meta: ['adverb', 'ноу', 'наречие', ''],
+    content: [
+      { type: 'title', text: 'no', speak: true },
+      { type: 'defaultSecondary', text: '/noʊ/' },
+      { type: 'defaultSemiBold', text: 'нет, не, ни, никакой, ни один' }
+    ]
+  },
+  {
+    id: 31,
+    category: ['A1'],
+    meta: ['verb', 'синк', 'глагол', ''],
+    content: [
+      { type: 'title', text: 'think', speak: true },
+      { type: 'defaultSecondary', text: '/θɪŋk/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'думать, считать, полагать, размышлять, мыслить'
+      }
+    ]
+  },
+  {
+    id: 32,
+    category: ['A1'],
+    meta: ['verb', 'вонт', 'глагол', ''],
+    content: [
+      { type: 'title', text: 'want', speak: true },
+      { type: 'defaultSecondary', text: '/wɒnt/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'хотеть, желать, стремиться, нуждаться, желать'
+      }
+    ]
+  },
+  {
+    id: 33,
+    category: ['A1'],
+    meta: ['verb', 'кам', 'глагол', ''],
+    content: [
+      { type: 'title', text: 'come', speak: true },
+      { type: 'defaultSecondary', text: '/kʌm/' },
+      {
+        type: 'defaultSemiBold',
+        text: 'приходить, приходить, приезжать, приходить, приходить'
+      }
+    ]
+  },
+  {
+    id: 34,
+    category: ['A1'],
+    meta: ['adverb', 'ап', 'наречие', ''],
+    content: [
+      { type: 'title', text: 'up', speak: true },
+      { type: 'defaultSecondary', text: '/ʌp/' },
+      { type: 'defaultSemiBold', text: 'вверх, наверх, вверху, вверх, на' }
+    ]
+  },
+  {
+    id: 35,
+    category: ['A1'],
+    meta: ['conjunction', 'иф', 'союз', ''],
+    content: [
+      { type: 'title', text: 'if', speak: true },
+      { type: 'defaultSecondary', text: '/ɪf/' },
+      { type: 'defaultSemiBold', text: 'если, ли, когда, хотя, пусть' }
+    ]
+  },
+  {
+    id: 36,
+    category: ['A1'],
+    meta: ['adverb', 'ес', 'наречие', ''],
+    content: [
+      { type: 'title', text: 'yes', speak: true },
+      { type: 'defaultSecondary', text: '/jɛs/' },
+      { type: 'defaultSemiBold', text: 'да, да, давай, конечно, давай, да' }
     ]
   }
+];
+
+const words_1_100 = [
+  'be',
+  'the',
+  'you',
+  'to',
+  'and',
+  'of',
+  'in',
+  'that',
+  'have',
+  'it',
+  'do',
+  'go',
+  'my',
+  'get',
+  'me',
+  'for',
+  'on',
+  'this',
+  'we',
+  'can',
+  'with',
+  'but',
+  'know',
+  'what',
+  'all',
+  'so',
+  'not',
+  'just',
+  'like',
+  'no',
+  'think',
+  'want',
+  'come',
+  'up',
+  'if',
+  'yes',
+  'out',
+  'when',
+  'they',
+  'at',
+  'say',
+  'as',
+  'see',
+  'one',
+  'make',
+  'he',
+  'about',
+  'there',
+  'now',
+  'time',
+  'love',
+  'from',
+  'right',
+  'take',
+  'here',
+  'look',
+  'will',
+  'person',
+  'or',
+  'how',
+  'thing',
+  'well',
+  'good',
+  'tell',
+  'feel',
+  'way',
+  'who',
+  'her',
+  'our',
+  'she',
+  'really',
+  'never',
+  'back',
+  'need',
+  'more',
+  'by',
+  'down',
+  'them',
+  'would',
+  'life',
+  'then',
+  'some',
+  'him',
+  'his',
+  'give',
+  'man',
+  'day',
+  'us',
+  'because',
+  'where',
+  'work',
+  'why',
+  'something',
+  'try',
+  'very',
+  'mean',
+  'only',
+  'let',
+  'their',
+  'leave'
 ];
