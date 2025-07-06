47230c6334fc9b9aee2e773e13329fecabf3e0d5
iLia Railian <raylyanway@mail.ru>
2025-07-06 23:39:55 +0000

    add wordTranslationMap

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-import { ICardType, ILearnedCardType } from '@/types';
+import { ICardType, ILearnedCardType, ITranslationLanguage } from '@/types';
 
 export const timeIntervals = [
   1000, // 1 second
@@ -42,4 +42,8 @@ export const timeIntervals = [
 export const learnedTypeMap: Record<ICardType, ILearnedCardType> = {
   card: 'learnedCards',
   word: 'learnedWords'
+};
+
+export const wordTranslationMap: Record<ITranslationLanguage, string> = {
+  RUS: 'слово'
 };
