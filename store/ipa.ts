7126e2255f3b12140dead3dc665c9c9cf72581fe
iLia Railian <raylyanway@mail.ru>
2025-07-07 04:44:09 +0000

    add ipa

---
+++ store/ipa.ts
@@ -0,0 +1,155 @@
+export const ipa = [
+  {
+    id: 1,
+    symbol: 'i',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "и"',
+    exampleRUS: 'мир',
+    exampleIpaRUS: 'mir',
+    exampleENG: 'see'
+  },
+  {
+    id: 2,
+    symbol: 'ɪ',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Ближе к "и", но немного смягчённее',
+    exampleRUS: '',
+    exampleIpaRUS: '',
+    exampleENG: 'bit'
+  },
+  {
+    id: 3,
+    symbol: 'e',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "е" под ударением',
+    exampleRUS: 'свет',
+    exampleIpaRUS: 'svet',
+    exampleENG: 'bait'
+  },
+  {
+    id: 4,
+    symbol: 'ɛ',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "э"',
+    exampleRUS: 'это',
+    exampleIpaRUS: 'ˈɛtə',
+    exampleENG: 'ten'
+  },
+  {
+    id: 5,
+    symbol: 'æ',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "а" в слове "сядь", но без "й"',
+    exampleRUS: 'сядь',
+    exampleIpaRUS: 'sʲatʲ',
+    exampleENG: 'cat'
+  },
+  {
+    id: 6,
+    symbol: 'ʌ',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "а" в безударной позиции',
+    exampleRUS: 'окно',
+    exampleIpaRUS: 'ɐˈkno',
+    exampleENG: 'cup'
+  },
+  {
+    id: 7,
+    symbol: 'ə',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "а" в безударной позиции',
+    exampleRUS: 'окно',
+    exampleIpaRUS: 'ɐˈkno',
+    exampleENG: 'about'
+  },
+  {
+    id: 8,
+    symbol: 'ɚ',
+    phoneme: 'vowel',
+    pronunciationRUS:
+      'Сложно передать, это "р" в середине слова после гласного',
+    exampleRUS: '',
+    exampleIpaRUS: '',
+    exampleENG: 'bird'
+  },
+  {
+    id: 9,
+    symbol: 'u',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "у"',
+    exampleRUS: 'ухо',
+    exampleIpaRUS: 'ˈuxə',
+    exampleENG: 'pool'
+  },
+  {
+    id: 10,
+    symbol: 'ʊ',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Ближе к "у", но немного мягче',
+    exampleRUS: '',
+    exampleIpaRUS: '',
+    exampleENG: 'book'
+  },
+  {
+    id: 11,
+    symbol: 'o',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "о" под ударением',
+    exampleRUS: 'дом',
+    exampleIpaRUS: 'dom',
+    exampleENG: 'go'
+  },
+  {
+    id: 12,
+    symbol: 'ɔ',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "о" в безударной позиции',
+    exampleRUS: '',
+    exampleIpaRUS: '',
+    exampleENG: 'caught'
+  },
+  {
+    id: 13,
+    symbol: 'ɑ',
+    phoneme: 'vowel',
+    pronunciationRUS: 'Звучит как "а"',
+    exampleRUS: 'папа',
+    exampleIpaRUS: 'ˈpapə',
+    exampleENG: 'father'
+  },
+  {
+    id: 14,
+    symbol: 'p',
+    phoneme: 'consonant',
+    pronunciationRUS: 'Звучит как "п"',
+    exampleRUS: 'парк',
+    exampleIpaRUS: 'park',
+    exampleENG: 'pot'
+  }
+];
+
+// b	Звучит как б	банк [bank] (bank)	bet, buy
+// t	Звучит как т	стол [stol] (table)	top, tall
+// d	Звучит как д	дом [dom] (house)	dog, dill
+// k	Звучит как к	кот [kot] (cat)	kit, cave
+// g	Звучит как г	год [got] (year)	got, give
+// f	Звучит как ф	факт [fakt] (fact)	fog, phone
+// v	Звучит как в	вода [vɐˈda] (water)	vat, video
+// θ	Нет точного эквивалента, похоже на глухой с, но с кончиком языка между зубами		thick, thin
+// ð	Нет точного эквивалента, похоже на звонкий з, но с кончиком языка между зубами		that, there
+// s	Звучит как с	сок [sok] (juice)	sat, celery
+// z	Звучит как з	зона [ˈzonə] (zone)	zoo, zebra
+// ʃ	Звучит как ш	шум [ʂum] (noise)	shut, shell
+// ʒ	Звучит как ж	жук [ʐuk] (beetle)	measure, genre
+// h	Звучит как х в начале слова	хлеб [xlʲɛp] (bread)	help, hill
+// t͡ʃ	Звучит как ч	час [t͡ʃʲas] (hour)	church, chip
+// d͡ʒ	Звучит как дж в слове "джаз"	джаз [d͡ʒas] (jazz)	jump, genre
+// m	Звучит как м	мама [ˈmama] (mother)	mom, mill
+// n	Звучит как н	нос [nos] (nose)	nod, nickel
+// ŋ	Звучит как н в слове "деньги"	деньги [ˈdʲenʲɡʲɪ] (money)	thing, singer
+// l	Звучит как л	лампа [ˈlampə] (lamp)	lot, lamb
+// r	Раскатистый р	рука [ruˈka] (hand)	rat, robot
+// w	Звучит как у после согласного		won, winter
+// j	Звучит как й	йод [jot] (iodine)	you, yellow
+// ʔ	Краткая остановка звука, нет точного эквивалента в русском		uh-oh
+// ɾ	Краткий звук, похожий на р		butter, pedal
