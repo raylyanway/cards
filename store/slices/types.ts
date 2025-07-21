804921367d1baa9cd76b3513388ae4cfb24c0984
iLia Railian <raylyanway@mail.ru>
2025-07-21 03:25:59 +0000

    update components

---
+++ store/slices/types.ts
@@ -0,0 +1,43 @@
+import { SQLiteDatabase } from 'expo-sqlite';
+
+import { ICard, ICardType, ILearnedCard, ITheme, IThemedColors } from '@/types';
+
+export type IAllSlices = IHydratedState &
+  IThemeSlice &
+  ICardsLearningSlice &
+  IDatabaseSlice;
+
+export interface IHydratedState {
+  _hasHydrated: boolean;
+  setHasHydrated: (state: boolean) => void;
+}
+
+export interface IThemeSlice {
+  theme: ITheme;
+  setTheme: (theme: ITheme) => void;
+  getColors: () => IThemedColors;
+  getIsLightTheme: () => boolean;
+  getOppositeColors: () => IThemedColors;
+}
+
+export interface ICardsLearningSlice {
+  currentCardIndex: number;
+  currentCardsType: ICardType;
+  currentCards: ICard[];
+  learnedCards: ILearnedCard[];
+  learnedWords: ILearnedCard[];
+  setCurrentCardIndex: (index: number) => void;
+  setCurrentCardsType: (cardType: ICardType) => void;
+  updateLearned: (cardId: number) => void;
+  refreshLearned: () => void;
+  getCurrentLearnedCards: () => ILearnedCard[];
+}
+
+export interface IDatabaseSlice {
+  _db: SQLiteDatabase | null;
+  getDb: () => SQLiteDatabase;
+  setDb: (database: SQLiteDatabase) => void;
+  getAllCustomers: () => Promise<any[]>;
+  getCustomerById: (id: number) => Promise<any | null>;
+  showAllTableData: () => Promise<void>;
+}
