de6aa2aa847359918431c2a5033786a4ba75417a
iLia Railian <raylyanway@mail.ru>
2025-07-21 03:24:24 +0000

    update components

---
+++ components/StatusBar.tsx
@@ -0,0 +1,12 @@
+import { StatusBar as RNStatusBar } from 'expo-status-bar';
+
+import 'react-native-reanimated';
+
+import { useBoundStore } from '@/store/useBoundStore';
+
+export const StatusBar = () => {
+  const isLightTheme = useBoundStore((state) => state.getIsLightTheme)();
+  const statusBarTheme = isLightTheme ? 'dark' : 'light';
+
+  return <RNStatusBar style={statusBarTheme} />;
+};
