fe091ce5fe0fdd52f4c34fbdf9290404dff35a16
iLia Railian <raylyanway@mail.ru>
2025-07-21 03:16:29 +0000

    add useBoundStore

---
+++ store/useBoundStore.ts
@@ -0,0 +1,36 @@
+import Storage from 'expo-sqlite/kv-store';
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+import { createCardsLearningSlice } from './slices/createCardsLearningSlice';
+import { createDatabaseSlice } from './slices/createDatabaseSlice';
+import { createThemeSlice } from './slices/createThemeSlice';
+import { IAllSlices } from './slices/types';
+
+export const useBoundStore = create<IAllSlices>()(
+  persist(
+    (set, ...a) => ({
+      ...createDatabaseSlice(set, ...a),
+      ...createThemeSlice(set, ...a),
+      ...createCardsLearningSlice(set, ...a),
+      _hasHydrated: false,
+      setHasHydrated: (state) => {
+        set({
+          _hasHydrated: state
+        });
+      }
+    }),
+    {
+      name: 'bound-storage',
+      storage: createJSONStorage(() => Storage),
+      onRehydrateStorage: (state) => {
+        return () => state.setHasHydrated(true);
+      },
+      partialize: (state) => ({
+        theme: state.theme,
+        learnedCards: state.learnedCards,
+        learnedWords: state.learnedWords
+      })
+    }
+  )
+);
