c6c308bfc8897807f2ac1755d6410b4867e43c7e
iLia Railian <raylyanway@mail.ru>
2025-07-21 03:20:45 +0000

    udpate settings

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -6,12 +6,12 @@ import { ScrollView } from '@/components/layouts/ScrollView';
 import { View } from '@/components/layouts/View';
 import { Switch } from '@/components/Switch';
 import { Text } from '@/components/texts/Text';
-import { useGlobalStore } from '@/store/useGlobalStore';
+import { useBoundStore } from '@/store/useBoundStore';
 
 export default function SettingsScreen() {
-  const colors = useGlobalStore((s) => s.computed.colors);
-  const isLightTheme = useGlobalStore((s) => s.computed.isLightTheme);
-  const setTheme = useGlobalStore((s) => s.setTheme);
+  const colors = useBoundStore((state) => state.getColors)();
+  const isLightTheme = useBoundStore((state) => state.getIsLightTheme)();
+  const setTheme = useBoundStore((state) => state.setTheme);
 
   const handleThemeToggle = () => {
     const newTheme = isLightTheme ? 'dark' : 'light';
