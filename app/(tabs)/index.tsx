e73e7e2c9cf1ecdd760f692f86de3bda45a389c5
iLia Railian <raylyanway@mail.ru>
2025-07-28 04:48:16 +0000

    update index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,13 +12,15 @@ import { useBoundStore } from '@/store/useBoundStore';
 
 export default function HomeScreen() {
   const learnedCards = useBoundStore((state) => state.learnedCards);
-  const showAllTableData = useBoundStore((state) => state.showAllTableData);
+  // const showAllTableData = useBoundStore((state) => state.showAllTableData);
+  // const showKVStore = useBoundStore((state) => state.showKVStore);
   const totalCards = cards.length;
 
   // logAllStorage(); // Log all storage for debugging
   // clearAllStorage(); // Clear storage for testing purposes
   // testDatabase();
-  showAllTableData();
+  // showAllTableData();
+  // showKVStore();
 
   const handleLearnPress = () => {
     router.push('/cards');
@@ -36,7 +38,7 @@ export default function HomeScreen() {
       >
         <View row spaceBetween>
           <Text>
-            Cards: {learnedCards.length} / {totalCards}
+            Cards: {learnedCards.size} / {totalCards}
           </Text>
           <BlockButton onPress={handleLearnPress}>Learn</BlockButton>
         </View>
