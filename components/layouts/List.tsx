b33354acc3b53fbdfa158d04a3197a9c3fa3ca1c
iLia Railian <raylyanway@mail.ru>
2025-08-04 03:27:36 +0000

    update List

--- components/layouts/List.tsx
+++ components/layouts/List.tsx
@@ -3,21 +3,26 @@ import React from 'react';
 import { HorizontalLine } from '@/components/HorizontalLine';
 
 import { Block, BlockProps } from './Block';
+import { View } from './View';
 
 export interface ListProps<T> extends BlockProps {
   list: T[];
   renderItem: (item: T) => React.ReactNode;
 }
 
-export const List = <T,>({ list, renderItem, ...blockProps }: ListProps<T>) => {
+export const List = <T extends { id: string }>({
+  list,
+  renderItem,
+  ...blockProps
+}: ListProps<T>) => {
   return (
     <Block {...blockProps}>
       {list.map((item, index) => {
         return (
-          <>
+          <View key={item.id}>
             {index !== 0 && <HorizontalLine />}
             {renderItem(item)}
-          </>
+          </View>
         );
       })}
     </Block>
