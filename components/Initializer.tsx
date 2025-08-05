5b196b1335106cd812c334b6035000f2b28ef801
iLia Railian <raylyanway@mail.ru>
2025-08-05 05:43:07 +0000

    update initializer

--- components/Initializer.tsx
+++ components/Initializer.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 
-import { timeIntervals } from '@/constants';
 import { useBoundStore } from '@/store/useBoundStore';
 
 interface InitializerProps {
@@ -11,10 +10,7 @@ export const Initializer = ({ children }: InitializerProps) => {
   const refreshLearned = useBoundStore((state) => state.refreshLearned);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      refreshLearned();
-    }, timeIntervals[0]);
-    return () => clearInterval(interval);
+    refreshLearned();
   }, [refreshLearned]);
 
   return <>{children}</>;
