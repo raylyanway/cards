376eb2fa41e237514ac77636e08ead04d289ec6f
iLia Railian <raylyanway@mail.ru>
2025-08-10 03:24:17 +0000

    update consts

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
 export const maxCardsToReview = 2;
-export const maxCardsToAdd = 10;
+export const maxCards = 10;
 export const week = 1000 * 60 * 60 * 24 * 7;
 export const timeIntervals = [
   1000 * 15, // 15 seconds
